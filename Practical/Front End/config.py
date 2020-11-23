3ab770ec87f028e217f29efdd8fdc85de10841d4
Krishna-Khanth <ajaykrishna1009@gmail.com>
2020-11-23 14:50:47 +0530

    cln

--- Practical/Front End/config.py
+++ Practical/Front End/config.py
@@ -1,27 +1,3 @@
-"""
-print (__name__)
-
-if __name__ == "config": #"__main__":
-    print ("HERE")
-    screen1 = "none"
-    screen1_5 = "none"
-    screen2 = "none"
-    screen3 = "none"
-    screen4 = "none"
-    screen5 = "none"
-
-    screen1geo = 0
-    screen1_5geo = 0
-    screen2geo = 0
-    screen3geo = 0
-    screen4geo = 0
-    screen5geo = 0
-    screen6geo = 0
-    screen7geo = 0
-
-    perm_entry = "none"
-"""
-
 screen1 = "none"
 screen1_5 = "none"
 screen2 = "none"
